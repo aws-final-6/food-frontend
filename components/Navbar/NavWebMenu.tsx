826f0258e400229035c20a23cd1c32432c03414a
Alice <jaeyeon0921@gmail.com>
2024-07-05 04:55:50 +0900

    create: navbar web menu list

---
+++ components/Navbar/NavWebMenu.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React, { useContext, useEffect, useState } from "react";
+import { link as linkStyles } from "@nextui-org/theme";
+import { NavbarItem } from "@nextui-org/navbar";
+import NextLink from "next/link";
+import clsx from "clsx";
+import { siteConfig } from "@/config/site";
+import { UserContext } from "@/providers/userProvider";
+import { usePathname } from "next/navigation";
+
+const NavWebMenu = () => {
+  const { isUserDataEmpty } = useContext(UserContext);
+  const pathname = usePathname();
+  const [navWebMenu, setNavWebMenu] = useState(siteConfig.webbasicItems);
+
+  useEffect(() => {
+    if (!isUserDataEmpty()) {
+      setNavWebMenu(siteConfig.webuserItems);
+    }
+  }, []);
+  return (
+    <>
+      <ul className="hidden lg:flex gap-12 justify-center ml-2">
+        {navWebMenu.map((item) => (
+          <NavbarItem key={item.href}>
+            <NextLink
+              className={clsx(
+                linkStyles({ color: "foreground" }),
+                "data-[active=true]:text-primary data-[active=true]:font-medium font-jua text-xl ",
+                {
+                  "border-b-3 border-gray-600": item.href == pathname,
+                }
+              )}
+              href={item.href}
+            >
+              {item.label}
+            </NextLink>
+          </NavbarItem>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default NavWebMenu;
