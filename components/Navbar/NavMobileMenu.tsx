3634b4cdcc28dd5c77b4cbe6e6e2c75b2f298c4c
Alice <jaeyeon0921@gmail.com>
2024-07-05 04:56:05 +0900

    create: navbar web menu list

---
+++ components/Navbar/NavMobileMenu.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React, { useState, useContext, useEffect } from "react";
+import { siteConfig } from "@/config/site";
+import { NavbarMenuItem } from "@nextui-org/navbar";
+import { Link } from "@nextui-org/link";
+import { UserContext } from "@/providers/userProvider";
+import { usePathname } from "next/navigation";
+
+const NavMobileMenu = () => {
+  const { isUserDataEmpty } = useContext(UserContext);
+  const pathname = usePathname();
+  const [navMobileMenu, setNavMobileMenu] = useState(
+    siteConfig.mobilebasicItems
+  );
+  useEffect(() => {
+    if (!isUserDataEmpty()) {
+      setNavMobileMenu(siteConfig.mobileuserItems);
+    }
+  }, []);
+  return (
+    <div className="mx-4 mt-2 flex flex-col gap-2">
+      {navMobileMenu.map((item, index) => (
+        <NavbarMenuItem key={`${item}-${index}`}>
+          <Link
+            color={
+              item.label == "로그아웃"
+                ? "danger"
+                : item.href == pathname
+                  ? "primary"
+                  : "foreground"
+            }
+            href={item.href}
+            size="lg"
+          >
+            {item.label}
+          </Link>
+        </NavbarMenuItem>
+      ))}
+    </div>
+  );
+};
+
+export default NavMobileMenu;
