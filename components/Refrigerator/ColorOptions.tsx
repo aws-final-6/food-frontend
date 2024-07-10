56731f494321a7ea636deae068acd7f704a7c065
Alice <jaeyeon0921@gmail.com>
2024-07-10 09:21:32 +0900

    create: color options

---
+++ components/Refrigerator/ColorOptions.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const colorOptions = [
+  {
+    bgcolor: "bg-norang dark:bg-norang-dark",
+    txtcolor: "text-norang dark:text-norang-dark",
+  },
+  {
+    bgcolor: "bg-mint dark:bg-mintdark",
+    txtcolor: "text-mint dark:text-mintdark",
+  },
+  {
+    bgcolor: "bg-yeondoo bg-yeondoodark",
+    txtcolor: "text-yeondoo text-yeondoodark",
+  },
+  {
+    bgcolor: "bg-haneul bg-haneuldark",
+    txtcolor: "text-haneul text-haneuldark",
+  },
+  { bgcolor: "bg-bora bg-boradark", txtcolor: "text-bora text-boradark" },
+  {
+    bgcolor: "bg-boonhong bg-boonhongdark",
+    txtcolor: "text-boonhong text-boonhongdark",
+  },
+];
