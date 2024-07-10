fc52d2496e3501fe2c3fcc8b5c71b4fbbd76bfe1
Alice <jaeyeon0921@gmail.com>
2024-07-10 09:21:41 +0900

    create: refrigerator column

--- components/Refrigerator/Column.tsx
+++ components/Refrigerator/Column.tsx
@@ -1,21 +1,30 @@
+"use client";
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
-import { IColumn, IIngredients } from "@/app/myrefrigerator/page";
+import { IRefrigerator, IIngredients } from "@/app/myrefrigerator/page";
 import StickyNote from "./StickyNote";
 import { Chip } from "@nextui-org/chip";
+import { CgTrash } from "react-icons/cg";
+import { deleteRefrigerator } from "./action";
+import { UserContext } from "@/providers/userProvider";
+import { useContext } from "react";
+import { useRefrigeratorContext } from "@/app/myrefrigerator/provider";
 
-const Column: React.FC<IColumn> = ({
-  refrigerator_name,
-  refrigerator_type,
-  refrigerator_id,
-  ingredients,
-}) => {
+interface ColumnProps {
+  refrigerator: IRefrigerator;
+}
+
+const Column: React.FC<ColumnProps> = ({ refrigerator }) => {
+  const { refrig, ingredients } = refrigerator;
+  const { userData } = useContext(UserContext);
+  const { setRefrig } = useRefrigeratorContext();
+
   function getStorageType(storage: number | null): string {
     switch (storage) {
-      case 0:
-        return "냉장";
       case 1:
-        return "냉동";
+        return "냉장";
       case 2:
+        return "냉동";
+      case 3:
         return "실온";
       default:
         throw new Error("Invalid storage type");
@@ -24,27 +33,39 @@ const Column: React.FC<IColumn> = ({
 
   function getColor(storage: number | null) {
     switch (storage) {
-      case 0:
-        return "primary";
       case 1:
-        return "secondary";
+        return "primary";
       case 2:
+        return "secondary";
+      case 3:
         return "warning";
       default:
         throw new Error("Invalid storage type");
     }
   }
+
+  async function deleteCol(id: number) {
+    const data = await deleteRefrigerator(id, userData[0].id);
+    setRefrig(data);
+  }
+
   return (
-    <Card className="w-full min-h-60" key={`ref_${refrigerator_id}`}>
+    <Card className="w-full min-h-60" key={`ref_${refrig.refrigerator_id}`}>
       <CardHeader className="flex flex-row items-center justify-center gap-3">
-        <p>{refrigerator_name}</p>
-        <Chip variant="flat" color={getColor(refrigerator_type)}>
-          {getStorageType(refrigerator_type)}
+        <p>{refrig.refrigerator_name}</p>
+        <Chip variant="flat" color={getColor(refrig.refrigerator_type)}>
+          {getStorageType(refrig.refrigerator_type)}
         </Chip>
+        <CgTrash
+          className="hover:text-red-500 hover:cursor-pointer z-10"
+          onClick={() => {
+            deleteCol(refrig.refrigerator_id);
+          }}
+        />
       </CardHeader>
       <CardBody className="grid sm:grid-cols-3 grid-cols-2 gap-4">
         {ingredients.map((ing: IIngredients, index: number) => (
-          <StickyNote {...ing} />
+          <StickyNote key={index} {...ing} />
         ))}
       </CardBody>
     </Card>
